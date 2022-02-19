4ed0e24fd7fb8c7c5fc636e7272cf609afc875f3
Jeffrey Yang <yangjeffrey14@gmail.com>
2022-02-18 19:19:00 -0500

    mobile compatible keyboard

--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -1,26 +1,25 @@
-import { motion } from 'framer-motion'
-import styled from 'styled-components'
-import { element } from '../types'
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import { element } from "../types";
 
-const Wrapper = styled.div``
+const Wrapper = styled.div``;
 
 const Row = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 3px;
   gap: 0 6px;
-`
+`;
 
 const Key = styled(motion.button)<{
-  length?: number
-  correct?: boolean
-  semiCorrect?: boolean
-  unUsed?: boolean
+  length?: number;
+  correct?: boolean;
+  semiCorrect?: boolean;
+  unUsed?: boolean;
 }>`
   font-family: inherit;
   font-weight: bold;
   border: 0;
-  padding: 0;
   height: 50px;
   border-radius: 4px;
   cursor: pointer;
@@ -36,40 +35,45 @@ const Key = styled(motion.button)<{
   text-transform: uppercase;
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0.3);
   width: ${(props) => (props.length ? (props.length - 1) * 8 + 40 : 40)}px;
+  max-width: ${(props) => (props.length ? (props.length - 1) * 8 + 40 : 40)}px;
   text-transform: uppercase;
   transition: 0.5s ease;
-`
 
-const row1 = 'Q w e r t y u i o p'.split(' ')
-const row2 = 'a s d f g h j k l'.split(' ')
-const row3 = 'enter z x c v b n m del'.split(' ')
+  @media screen and (max-width: 480px) {
+    width: ${(props) => (props.length ? (props.length - 1) * 8 + 25 : 25)}px;
+  }
+`;
+
+const row1 = "q w e r t y u i o p".split(" ");
+const row2 = "a s d f g h j k l".split(" ");
+const row3 = "enter z x c v b n m del".split(" ");
 
 export default ({
   answer,
   grid,
   key,
 }: {
-  answer: string
-  grid: element[]
-  key: string
+  answer: string;
+  grid: element[];
+  key: string;
 }) => {
-  const arr = grid.filter(({ lock }) => lock).map(({ letter }) => letter)
-  console.log(key)
+  const arr = grid.filter(({ lock }) => lock).map(({ letter }) => letter);
+  console.log(key);
   const unUsed = (letter: string) => {
-    return arr.some((l) => letter == l)
-  }
+    return arr.some((l) => letter == l);
+  };
 
   const correct = (letter: string) => {
-    const answerPos = answer.indexOf(letter)
-    const wordPos = arr.indexOf(letter)
-    return wordPos % 5 === answerPos % 5 && wordPos != -1
-  }
+    const answerPos = answer.indexOf(letter);
+    const wordPos = arr.indexOf(letter);
+    return wordPos % 5 === answerPos % 5 && wordPos != -1;
+  };
 
   const semiCorrect = (letter: string) => {
-    const answerPos = answer.indexOf(letter)
-    const wordPos = arr.indexOf(letter)
-    return wordPos != -1 && answerPos != -1
-  }
+    const answerPos = answer.indexOf(letter);
+    const wordPos = arr.indexOf(letter);
+    return wordPos != -1 && answerPos != -1;
+  };
   return (
     <Wrapper>
       <Row>
@@ -114,5 +118,5 @@ export default ({
         </Row>
       </Row>
     </Wrapper>
-  )
-}
+  );
+};
